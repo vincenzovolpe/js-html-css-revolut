b5282cf6652a04c0a4a9fbd943d14f080f7a4b72
vincenzovolpe <vinci792010@gmail.com>
2019-11-27 00:35:21 +0100

    Caricamento esercizio completo

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 $("document").ready(function() {
-    $(".menu-item").mouseover(function() {
-        //$(this).children('a').addClass('.hover');
-        $(this).children('.dropdown-menu').stop().slideDown(300);
-    })
-    $(".menu-item").mouseout(function() {
-        //$(this).children('a').removeClass('.hover');
-        $(this).children('.dropdown-menu').stop().slideUp(300);
-    })
-
+        $(".menu-item").mouseover(function() {
+            if (!$(this).hasClass("lingua")) {
+                $(this).children("#attivo").addClass("colore");
+                $(this).children('.dropdown-menu').stop().slideDown(300);
+            } else {
+                $(this).children("a").children("span").removeClass("fa fa-angle-down");
+                $(this).children("a").children("span").addClass("fa fa-angle-up");
+                    $(this).children("#attivo").addClass("colore");
+                $(this).children('.dropdown-menu').stop().slideDown(300);
+            }
+        })
+            $(".menu-item").mouseout(function() {
+                if (!$(this).hasClass("lingua")) {
+                    $(this).children("#attivo").removeClass("colore");
+                    $(this).children('.dropdown-menu').stop().slideUp(300);
+                } else {
+                    $(this).children("a").children("span").removeClass("fa fa-angle-up");
+                    $(this).children("a").children("span").addClass("fa fa-angle-down");
+                    $(this).children("#attivo").removeClass("colore");
+                    $(this).children('.dropdown-menu').stop().slideUp(300);
+            }
+            })
 })
