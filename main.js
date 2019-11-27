75d38b5da083a0c9c5f1635f02a9bfe30d242507
vincenzovolpe <vinci792010@gmail.com>
2019-11-27 09:21:15 +0100

    Aggiustata condizione che controolla ultimo elemento del menu di destra nel jquery

--- main.js
+++ main.js
@@ -1,24 +1,20 @@
 $("document").ready(function() {
         $(".menu-item").mouseenter(function() {
-            if (!$(this).hasClass("lingua")) {
-                $(this).children("#attivo").addClass("colore");
-                $(this).children('.dropdown-menu').stop().slideDown(300);
-            } else {
+            if ($(this).hasClass("lingua")) {
                 $(this).children("a").children("span").removeClass("fa fa-angle-down");
                 $(this).children("a").children("span").addClass("fa fa-angle-up");
-                    $(this).children("#attivo").addClass("colore");
-                $(this).children('.dropdown-menu').stop().slideDown(300);
             }
+            $(this).children("#attivo").addClass("colore");
+            $(this).children('.dropdown-menu').stop().slideDown(300);
+
         })
             $(".menu-item").mouseleave(function() {
-                if (!$(this).hasClass("lingua")) {
-                    $(this).children("#attivo").removeClass("colore");
-                    $(this).children('.dropdown-menu').stop().slideUp(300);
-                } else {
+                if ($(this).hasClass("lingua")) {
                     $(this).children("a").children("span").removeClass("fa fa-angle-up");
                     $(this).children("a").children("span").addClass("fa fa-angle-down");
-                    $(this).children("#attivo").removeClass("colore");
-                    $(this).children('.dropdown-menu').stop().slideUp(300);
-            }
+                }
+                $(this).children("#attivo").removeClass("colore");
+                $(this).children('.dropdown-menu').slideUp(300);
+
             })
 })
